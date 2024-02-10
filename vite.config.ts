9d30e6d11ecfe9b11e040b800aee7a679b9f946b
Marek Procházka <m.prochazka2002@gmail.com>
2024-02-10 18:27:01 +0100

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import vue from "@vitejs/plugin-vue";
+// @ts-ignore
 import * as path from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
@@ -7,9 +8,10 @@ export default defineConfig({
   plugins: [vue(), dts()],
   build: {
     lib: {
+      // @ts-ignore
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "vCMS",
-      fileName: "v-cms"
+      fileName: "v-cms",
     },
     rollupOptions: {
       external: ["vue"],
@@ -22,6 +24,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      // @ts-ignore
       "@": path.resolve(__dirname, "src")
     }
   }
