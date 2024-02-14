c35cf61a6f80d99ae765f777cb646106254c6bcf
Marek Procházka <m.prochazka2002@gmail.com>
2024-02-14 07:48:19 +0100

    basic editor

--- src/core/components/editor.vue
+++ src/core/components/editor.vue
@@ -5,9 +5,37 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 import type { EditorConfig } from '@ckeditor/ckeditor5-core'
 import { ref } from 'vue'
 
+const props = defineProps<{
+  customConfig?: EditorConfig
+}>()
+
 const editor = CK.component
 
-const config: EditorConfig = {}
+const config: EditorConfig = {
+  toolbar: {
+    items: [
+      'undo', 'redo',
+      '|', 'heading',
+      // '|', 'fontColor', 'fontBackgroundColor', 'strikethrough', TBD
+      '|', 'bold', 'italic',
+      '|', 'link', 'blockQuote',
+      // '|', 'uploadImage', TBD
+      '|', 'bulletedList', 'numberedList',
+      // 'todoList', 'outdent', 'subscript', 'superscript', 'code', 'indent'  TBD
+    ]
+  },
+  heading: {
+    options: [
+      { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+      { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+      { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+      { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+      { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+      { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+      { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+    ]
+  },
+    ...props.customConfig}
 
 
 const data = ref('<p>Bonjour from hello world</p>')
@@ -15,8 +43,12 @@ const data = ref('<p>Bonjour from hello world</p>')
 </script>
 
 <template>
-  <div>{{data}}</div>
   <editor :editor="ClassicEditor" :config="config" v-model="data" />
 </template>
 
+<style>
+.ck-editor__editable_inline {
+    min-height: 400px !important;
+}
+</style>
 
