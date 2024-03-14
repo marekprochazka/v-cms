d695f158147c740d9083f6382ef28d3261eb3bcb
Marek Procházka <m.prochazka2002@gmail.com>
2024-03-14 07:52:21 +0100

    core exports, bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+### 0.1.11
+- added interface exports to 'core' module
+
 ### 0.1.2
 - added homepage link to package.json
 
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,8 @@
 import { useCMS } from './composables/useCMS'
 import Editor from './components/editor/wrapper.vue'
 import ContentBody from './components/content/body.vue'
+import {CRUD, IContentBase, IComponentInternal, IContentDetailProps, IEditorProps } from './types'
 
 export { useCMS, Editor, ContentBody }
+
+export type { CRUD,  IContentBase, IContentDetailProps, IComponentInternal, IEditorProps}
